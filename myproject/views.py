185c82ebe56eda3de574762a420d1c8f55068ac8
tuya <tuya@ukr.net>
2019-12-11 20:58:55 +0200

    add app cities

--- myproject/views.py
+++ myproject/views.py
@@ -1,16 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_view(request):
-    html = ''' <!DOCTYPE html>
-        <html>
-        <head>
-        <title>Title of document</title>
-        </head>
-        
-        <body>
-        <h1> Hello World!</h1>
-        </body>
-        
-        </html>'''
-    return HttpResponse(html)
+    context = {'name': 'Bob'}
+    return render(request, 'home.html', context)
