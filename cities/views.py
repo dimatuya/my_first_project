165a5b8cf16f870e1f38f2db97ab03034a1c436f
tuya <tuya@ukr.net>
2019-12-18 11:24:35 +0200

    improve app cities

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import City
+
+def home(request, pk=None):
+    if pk:
+        city = City.objects.filter(pk=pk).first()
+        return render(request, 'cities/detail.html', {'object': city, })
+    cities = City.objects.all()
+    return render(request, 'cities/home.html', {'objects_list': cities, })
 
 # Create your views here.
